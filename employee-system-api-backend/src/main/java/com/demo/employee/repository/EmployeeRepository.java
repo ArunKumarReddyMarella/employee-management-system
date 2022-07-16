fa604ecac6bb01ce65feffd8dd62728e7a987b02
ArunKumarReddyMarella <arunkumarreddy159@gmail.com>
2022-07-17 00:10:47 +0530

    final

---
+++ employee-system-api-backend/src/main/java/com/demo/employee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.demo.employee.repository;
+
+import com.demo.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+}
---
+++ employee-system-api-frontend/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import EmployeeList from "./components/EmployeeList";
+import Navbar from './components/Navbar';
+import UpdateEmployee from "./components/UpdateEmployee";
+
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<EmployeeList />}></Route>
+        <Route index element={<EmployeeList />}></Route>
+        <Route path="/employeeList" element={<EmployeeList />}></Route>
+        <Route path="/addEmployee" element={<AddEmployee />}></Route>
+        <Route path="/updateEmployee/:id" element={<UpdateEmployee />}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
